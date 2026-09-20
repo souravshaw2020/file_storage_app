0d8cb195a53ed9eddd2e081f66318ccbb3116123
Sourav Shaw <sourav.shaw@st.niituniversity.in>
2026-09-20 18:00:39 +0530

    Update Client API with backend URL

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || '/api',
+  baseURL: `${process.env.BACKEND_ORIGIN}${process.env.NEXT_PUBLIC_API_URL}` || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
